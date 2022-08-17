61c80a1a243258a6f4e55a644eb2b69f28979beb
DanDan <ddaneshfar@my.bcit.ca>
2022-08-17 11:33:47 -0700

    Made a counter with React

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
-
+import {useState} from 'react';
 import './App.css';
 
-const Person = (props)=> {
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastname}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
 const App = ()=> {
-  const name = 'Dssn';
-  const isNameShowing = true;
-  const isUserLoggedIn = true;
-  return (
-    <div className="App">
-      <h1> Hello {isNameShowing ? name : 'someone'}!</h1>
-      {name ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) : (
-        <>
-        <h2> test </h2>
-        <h3> There is no name </h3>
-        </>
-      )}
+const [counter, setCounter] =useState(0);
 
-      <Person name={'John'} lastname={'Doe'} age={23}/>
-      <Person name={'Jane'} lastname={'Don'} age={23}/>
-      <Person name={'Josh'} lastname={'Doe'} age={23}/>
-      <Person name={'Jack'} lastname={'Doe'} age={23}/>
-      <Person name={'Jacklyn'} lastname={'Doe'} age={23}/>
-    </div>
+return(
+
+  <div className="App">
+    <button onClick={()=>setCounter((prevCount)=> prevCount - 1)}>-</button>
+    <h1>{counter}</h1>
+    <button onClick={()=>setCounter((prevCount)=> prevCount + 1)}>+</button>
+  </div>
   );
+
+  
 }
 
 export default App;
